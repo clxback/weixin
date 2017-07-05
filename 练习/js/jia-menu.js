d98ab6e138a4008bd6069e5050d1c039ce8a654d
Administrator <Administrator@STU-19>
2017-07-05 14:01:28 +0800

    单击加号弹出窗口！

--- 练习/js/jia-menu.js
+++ 练习/js/jia-menu.js
@@ -1,13 +1,24 @@
 
 
+
 $(function(){
+	
+	
+	
+//	/*  控制顶层弹出菜单层 : start  */
+//	/* 弹出层  和 弹出小三角   初始应该为 隐藏  */
 	$("#jia-menu").hide();
-	$('#add').CLICK(function(){
+
+	$('#add').click(function(){
 		$("#jia-menu").toggle(200);
 	})
-	$("#jia-menu").CLICK(function(){
-		$("#jia-menu").hide();
+	/*  控制顶层弹出菜单层 : end  */
+	
+	// 点击弹出层本身   也会消失
+	$("#jia-menu").click(function(){
+		$("#jia-menu ").hide();
 	});
+	
+	
+	
 })
-
-
