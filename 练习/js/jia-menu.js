6a4e3b39dcc491d4e1a9d36b75de18ccdd720659
Administrator <Administrator@STU-19>
2017-07-05 11:29:35 +0800

    哭死

---
+++ 练习/js/jia-menu.js
@@ -0,0 +1,13 @@
+
+
+$(function(){
+	$("#jia-menu").hide();
+	$('#add').CLICK(function(){
+		$("#jia-menu").toggle(200);
+	})
+	$("#jia-menu").CLICK(function(){
+		$("#jia-menu").hide();
+	});
+})
+
+
